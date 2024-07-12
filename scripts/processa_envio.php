5f4b4d0e811425005b2bded5ef7881142f21f7c7
Mateus Araújo <f.mateusaraujo20@gmail.com>
2024-07-11 22:35:59 -0300

    add library phpmailer

--- scripts/processa_envio.php
+++ scripts/processa_envio.php
@@ -1,6 +1,12 @@
 <?php 
 
 require_once '../class/Message.php';
+require '../library/PHPMailer/Exception.php';
+require '../library/PHPMailer/OAuthTokenProvider.php';
+require '../library/PHPMailer/PHPMailer.php';
+require '../library/PHPMailer/OAuth.php';
+require '../library/PHPMailer/POP3.php';
+require '../library/PHPMailer/SMTP.php';
 
 
 
